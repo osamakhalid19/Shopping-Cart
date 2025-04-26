3437d82895832cebae738e702e3383c1bc6be337
osamakhalid19 <osamakhalid1890@gmail.com>
2025-04-26 06:06:21 -0700

    Update Context.jsx

--- vite-project/src/assets/Components/Context.jsx
+++ vite-project/src/assets/Components/Context.jsx
@@ -1,17 +1,34 @@
-import { createContext,useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const context = createContext();
 
-const ContextProvider = ({children}) =>{
-const [cart, setcart] = useState([])
-const [quantity, setquantity] = useState(1)
+const ContextProvider = ({ children }) => {
 
-    return <>
-     <context.Provider value={{cart,setcart,setquantity,quantity}}>
-    {children}
-    </context.Provider>
-    </>
+    const [cart, setcart] = useState(() => {
+        try {
+            const content = localStorage.getItem("cart")
+
+            if(content && content !== undefined){
+                return JSON.parse(content)
+            }
+            return []
+
+        } catch (error) {
+            console.log("Error ocuurs")
+            return[]
+        }
+    });
+
+    useEffect(() => {
+        localStorage.setItem("cart", JSON.stringify(cart));
+    }, [cart]);
+
+    return (
+        <context.Provider value={{ cart, setcart }}>
+            {children}
+        </context.Provider>
+    );
 }
 
-export default context 
-export {ContextProvider}
+export default context;
+export { ContextProvider };
